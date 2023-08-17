362a20ef0609e98f36f02d3eb5557f9371c6da48
rick <g539983038@gmail.com>
2023-08-17 17:32:25 +0800

    gpw

--- src/localization_node.cpp
+++ src/localization_node.cpp
@@ -67,7 +67,7 @@ static bool exit_initial_thread = false;
 
 static std::string localization_status = "fail";
 static std::string current_map_name = "";
-static std::string status_file_dir = "/home/justin/zhenrobot/.loc_status/";
+static std::string status_file_dir = "/home/justin/zhenrobot/.status/";
 static std::string map_file_dir = "/home/justin/zhenrobot/map/";
 
 Eigen::Matrix4d T_odom_to_map, T_wheel_odom_to_map, T_base_to_map, T_base_to_odom, T_base_to_wheel_odom,
